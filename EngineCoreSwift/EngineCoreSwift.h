33f03addff89feefe8e7aa2068c3e7ed3f582c55
shravangudikadi1 <94861850+shravangudikadi1@users.noreply.github.com>
2022-05-11 15:41:26 -0500

    Initial Commit

---
+++ EngineCoreSwift/EngineCoreSwift.h
@@ -0,0 +1,18 @@
+//
+//  EngineCoreSwift.h
+//  EngineCoreSwift
+//
+//  Created by Shravan Gudikadi on 5/10/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EngineCoreSwift.
+FOUNDATION_EXPORT double EngineCoreSwiftVersionNumber;
+
+//! Project version string for EngineCoreSwift.
+FOUNDATION_EXPORT const unsigned char EngineCoreSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EngineCoreSwift/PublicHeader.h>
+
+
---
+++ EngineCoreSwift/Sample.swift
@@ -0,0 +1,18 @@
+//
+//  Sample.swift
+//  EngineCoreSwift
+//
+//  Created by Shravan Gudikadi on 5/10/22.
+//
+
+import Foundation
+import UIKit
+import EngineUIKit
+
+public struct Sample {
+    
+    public func EngineButton(frame: CGRect?, title: String?, state: UIControl.State?, font: UIFont?, color: UIColor?) { }
+    
+    
+}
+
